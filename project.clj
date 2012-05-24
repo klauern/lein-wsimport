2926d2c093be7b351ca9df6a6e10336b61b879de
Nick Klauer (a03182) <nicholasklauer@alliantenergy.com>
2012-05-24 16:18:58 -0500

    small changes
    Start figuring out what needs to be called and how to call it.

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject lein-wsimport "1.0.0-SNAPSHOT"
   :description "JAX-WS Import plugin for Clojure projects"
   :url ""
-  :dependencies [[com.sun.xml.ws/jaxws-tools "2.2.7-promoted-b73"]]
+  :dependencies [[com.sun.xml.ws/jaxws-tools "2.2.7-promoted-b73"]
+                 [com.sun/tools ""]]
+  :java-source-paths ["src/main/java"] ; Java source is stored separately.
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
--- src/leiningen/wsimport.clj
+++ src/leiningen/wsimport.clj
@@ -1,10 +1,10 @@
 (ns leiningen.wsimport
   (:import (com.sun.tools.ws.WsImport)))
 
-(def default-opts (atom {}))
-
+(def default-opts (atom ["-Xnocompile" "-d" "src/main/java"]))
 
 (defn wsimport
   "I don't do a lot."
   [project & args]
-  (println "Hi!"))
+  ;; ??  learn the project arguments for this, figure out what needs to be gotten
+  (WsImport/doMain (into-array default-opts)))
