59ce9f2fa7278a2de173ac39c8aa535dbad88e6f
Nick Klauer (a03182) <nicholasklauer@alliantenergy.com>
2012-05-25 14:18:10 -0500

    convert options as an array to pass in
    I need to convert the default atom [] to be a more composable set of
    options.  I don't know exactly how this will work, but I expect that I'd
    provide a map of default options, and then be able to run through the
    passed in args and project settings and mush them together.
    
    Then, finally, I'd take that default set of options and convert that to
    an array of options to the WsImport class.  Neat stuff this Clojure
    language.

--- src/leiningen/wsimport.clj
+++ src/leiningen/wsimport.clj
@@ -2,12 +2,19 @@
   (:require [clojure.java.io :as io]) 
   (:import (com.sun.tools.ws WsImport)))
 
-(def opts (atom {:-Xnocompile true, :-d "target/generated/java"})) 
+(def opts (atom {:compile-java-sources false
+                 :output-directory "target/generated/java"
+                 })) 
 (def default-opts (atom ["-Xnocompile" "-d" "target/generated/java"]))
-(defn map-project-onto-opts
-  "Map the project settings onto the existing default opts"
-  [opts]
-  )
+(defn compose-options-array
+  "Create the options array to pass to make the wsimport call,
+   using both the project options (if specified), or just the argument
+   listing" 
+  ([project]
+    
+    )
+  ([project & args]
+    )) 
 
 (defn wsimport
   "I don't do a lot."
@@ -19,7 +26,7 @@
     ;; make the output directory if it doesn't already exist
     ;; call doMain with options
     (do
-      (let [f (clojure.java.io/file "target/generated/java/")]
+      (let [f (clojure.java.io/file (:output-directory opts))]
         (if-not (.exists f)
         (.mkdirs f)))
       (WsImport/doMain (into-array 
