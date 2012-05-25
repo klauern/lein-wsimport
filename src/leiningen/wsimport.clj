b5df4e2052ac5728d3327d94507130c7044b6036
Nick Klauer (a03182) <nicholasklauer@alliantenergy.com>
2012-05-25 15:01:07 -0500

    add more options (need tests)
    I'm adding more options, but I need to learn how to test this thing.
    Since it's not using any state, this should be easily testable, but I
    don't know much about unit testing, midje or any of that fancy stuff.
    Gonna have to dig and learn things here.

--- src/leiningen/wsimport.clj
+++ src/leiningen/wsimport.clj
@@ -2,8 +2,10 @@
   (:require [clojure.java.io :as io]) 
   (:import (com.sun.tools.ws WsImport)))
 
+
 (def opts (atom {:compile-java-sources false
-                 :output-directory "target/generated/java"
+                 :java-output-directory "target/generated/java"
+                 :keep-java-sources true
                  })) 
 (def default-opts (atom ["-Xnocompile" "-d" "target/generated/java"]))
 (defn compose-options-array
@@ -15,9 +17,14 @@
     (let [array []]
       (do
         (if-not (:compile-java-sources opts) (conj array "-Xnocompile"))
-        (if-let [out-dir (-> project :wsimport :output-directory)]
-          (conj array "-d" out-dir)
-          (conj array "-d" (:output-directory opts)))
+        (if-let [out-dir (-> project :wsimport :java-output-directory)]
+          (conj array "-s" out-dir)
+          (conj array "-s" (:java-output-directory opts)))
+        (if-let [keep (-> project :wsimport :keep-java-sources)]
+          (if keep
+            (conj array "-keep"))
+          (if (:keep-java-sources opts)
+            (conj array "-keep")))
         ))
     )
   ([project & args]
@@ -39,3 +46,55 @@
       (WsImport/doMain (into-array 
                          (swap! default-opts
                                 (fn [dopts] (vec (concat dopts args)))))))))
+
+;; lots of potential options.  I don't see a need for many of them, but I'll probably
+;; just wrap a couple of them:
+;;   - -b for jaxws/jaxb binding files
+;;   - httpproxy (should be implicit)
+;;   - keep ? (this is kind of working already with the -Xnocompile, might have to be explicit
+;;   - -p pkg
+;;   - quiet
+;;   - -s directory for source files
+;;   - 
+;; Usage: wsimport [options] <WSDL_URI>
+;; 
+;; where [options] include:
+;;   -b <path>                 specify jaxws/jaxb binding files or additional schemas
+;;                             (Each <path> must have its own -b)
+;;   -B<jaxbOption>            Pass this option to JAXB schema compiler
+;;   -catalog <file>           specify catalog file to resolve external entity references
+;;                             supports TR9401, XCatalog, and OASIS XML Catalog format.
+;;   -d <directory>            specify where to place generated output files
+;;   -extension                allow vendor extensions - functionality not specified
+;;                             by the specification.  Use of extensions may
+;;                             result in applications that are not portable or
+;;                             may not interoperate with other implementations
+;;   -help                     display help
+;;   -httpproxy:<host>:<port>  specify a HTTP proxy server (port defaults to 8080)
+;;   -keep                     keep generated files
+;;   -p <pkg>                  specifies the target package
+;;   -quiet                    suppress wsimport output
+;;   -s <directory>            specify where to place generated source files
+;;   -target <version>         generate code as per the given JAXWS spec version
+;;                             Defaults to 2.2, Accepted values are 2.0, 2.1 and 2.2
+;;                             e.g. 2.0 will generate compliant code for JAXWS 2.0 spec
+;;   -verbose                  output messages about what the compiler is doing
+;;   -version                  print version information
+;;   -wsdllocation <location>  @WebServiceClient.wsdlLocation value
+;;   -clientjar <jarfile>      Creates the jar file of the generated artifacts along with the
+;;                             WSDL metadata required for invoking the web service.
+;; 
+;; Extensions:
+;;   -XadditionalHeaders              map headers not bound to request or response message to
+;;                                    Java method parameters
+;;   -Xauthfile                       file to carry authorization information in the format
+;;                                    http://username:password@example.org/stock?wsdl
+;;   -Xdebug                          print debug information
+;;   -Xno-addressing-databinding      enable binding of W3C EndpointReferenceType to Java
+;;   -Xnocompile                      do not compile generated Java files
+;;   -XdisableSSLHostnameVerification disable the SSL Hostname verification while fetching
+;;                                    wsdls
+;; 
+;; Examples:
+;;   wsimport stock.wsdl -b stock.xml -b stock.xjb
+;;   wsimport -d generated http://example.org/stock?wsdl
