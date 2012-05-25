b0c4f3f5e2496a741492f74f70ec8b12d385a193
Nick Klauer (a03182) <nicholasklauer@alliantenergy.com>
2012-05-25 10:12:03 -0500

    process command-line args for wsimport task
    This more or less proxies the WsImport task in JDK, but merges a couple
    default
    options to the set of arguments (no compilation and specify output
    directory
    to target/generated/java)

--- src/leiningen/wsimport.clj
+++ src/leiningen/wsimport.clj
@@ -1,13 +1,21 @@
 (ns leiningen.wsimport
-  (:import (com.sun.tools.ws.WsImport)))
+  (:import (com.sun.tools.ws WsImport)))
 
-(def default-opts (atom ["-Xnocompile" "-d" "src/main/java"]))
+(def default-opts (atom ["-Xnocompile" "-d" "target/generated/java"]))
+(defn map-project-onto-opts
+  "Map the project settings onto the existing default opts"
+  [opts]
+  )
 
 (defn wsimport
   "I don't do a lot."
   ([project]
-  ;; ??  learn the project arguments for this, figure out what needs to be gotten
-  (WsImport/doMain (into-array default-opts)))
+    ;; ??  learn the project arguments for this, figure out what needs to be gotten
+    (WsImport/doMain (into-array @default-opts)))
   ([project & args]
     ;; process command-line options... (compile directly?)
-    (WsImport/doMain (into-array default-opts args))))
+    ;; make the output directory if it doesn't already exist
+    ;; call doMain with options
+    (WsImport/doMain (into-array 
+                       (swap! default-opts
+                              (fn [dopts] (vec (concat @dopts args))))))))
