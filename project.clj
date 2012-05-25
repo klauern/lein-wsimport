c4ec4faf88cd1ba18cde3921f1bb043c138d7df9
Nick Klauer (a03182) <nicholasklauer@alliantenergy.com>
2012-05-25 10:13:51 -0500

    remove bad dependency
    bad mistake committed to the project settings on my part.  We don't even
    need this dep.

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject lein-wsimport "1.0.0-SNAPSHOT"
   :description "JAX-WS Import plugin for Clojure projects"
   :url ""
-  :dependencies [[com.sun.xml.ws/jaxws-tools "2.2.7-promoted-b73"]
-                 [com.sun/tools ""]]
-  :java-source-paths ["src/main/java"] ; Java source is stored separately.
+  :dependencies [[com.sun.xml.ws/jaxws-tools "2.2.7-promoted-b73"]]
+  :java-source-paths ["target/generated/java"] ; Java source is stored separately.
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
