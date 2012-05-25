ee5381a10af5f7be762f8afb012ad9427923938b
Nick Klauer <klauer@gmail.com>
2012-05-25 05:56:11 -0500

    destructuring main opts

--- src/leiningen/wsimport.clj
+++ src/leiningen/wsimport.clj
@@ -5,6 +5,9 @@
 
 (defn wsimport
   "I don't do a lot."
-  [project & args]
+  ([project]
   ;; ??  learn the project arguments for this, figure out what needs to be gotten
   (WsImport/doMain (into-array default-opts)))
+  ([project & args]
+    ;; process command-line options... (compile directly?)
+    (WsImport/doMain (into-array default-opts args))))
