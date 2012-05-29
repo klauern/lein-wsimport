eb7f7dc9be5bd45f4a90e133c1eaf60ea49c272a
Nick Klauer <klauer@gmail.com>
2012-05-28 21:47:19 -0500

    add EC2 WSDL test file

--- test/leiningen/wsimporting_tests.clj
+++ test/leiningen/wsimporting_tests.clj
@@ -2,4 +2,7 @@
   (:use midje.sweet)
   (:use leiningen.wsimport))
 
+(def ec2-service "test/resources/ec2.wsdl")
 
+(fact "testing import of Amazon EC2 WSDL service"
+      (fail "Not gonna work on it yet."))
