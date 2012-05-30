cacc7ee531c4d22884635cdf28dd54b7f65d818b
Nick Klauer (a03182) <nicholasklauer@alliantenergy.com>
2012-05-30 14:12:03 -0500

    Add some extra WSDL locations for future use
    I don't know what to test or how to test it, but I'm going to build up a
    list of possible WSDL examples to test against.

--- test/leiningen/wsimporting_tests.clj
+++ test/leiningen/wsimporting_tests.clj
@@ -6,7 +6,17 @@
             [clojure.java.io :as io]))
 
 (def ec2-wsdl (io/resource "test/resources/ec2.wsdl"))
+(def s3-wsdl "http://s3.amazonaws.com/doc/2006-03-01/AmazonS3.wsdl")
+(def sdb-wsdl "http://sdb.amazonaws.com/doc/2009-04-15/AmazonSimpleDB.wsdl")
+(def mturk-wsdl "http://mechanicalturk.amazonaws.com/AWSMechanicalTurk/2012-03-25/AWSMechanicalTurkRequester.wsdl")
+(def sqs-wsdl "http://queue.amazonaws.com/doc/2011-10-01/QueueService.wsdl")
 
+
+
+(def ebay-finding-svc "http://developer.ebay.com/webservices/finding/latest/FindingService.wsdl")
+(def ebay-trading-svc "http://developer.ebay.com/webservices/latest/ebaySvc.wsdl")
+
+(def ms-search-wsdl "http://api.search.live.net/search.wsdl")
 
 ;; (fact "testing import of Amazon EC2 WSDL service"
 ;;      (fail "Not gonna work on it yet."))
